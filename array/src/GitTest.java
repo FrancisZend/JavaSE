b62f70e692829bda54469d7da3b72281133e56c5
ZengQiang <1340073429@qq.com>
2021-07-10 21:37:36 +0800

    hot-fix分支第2次提交

--- array/src/GitTest.java
+++ array/src/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git hot-fix1");
+        System.out.println("hello git hot-fix2");
 
     }
 }
