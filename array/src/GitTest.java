83240bcb602378f09cb2537355bcb8586f4cfab2
FrancisZend <73213796+FrancisZend@users.noreply.github.com>
2021-07-11 00:05:16 +0800

    Update GitTest.java

--- array/src/GitTest.java
+++ array/src/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git hot-fix1");
         System.out.println("hello git hot-fix2");
         System.out.println("push to remote server");
+        System.out.println("pull from  remote server");
 
     }
 }
