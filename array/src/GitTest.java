49f9f0a13a579400411caf2982a46edfbcba60ca
ZengQiang <1340073429@qq.com>
2021-07-10 23:40:35 +0800

    测试推送到仓库

--- array/src/GitTest.java
+++ array/src/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git hot-fix1");
         System.out.println("hello git hot-fix2");
+        System.out.println("push to remote server");
 
     }
 }
